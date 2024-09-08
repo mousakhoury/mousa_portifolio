c02daf235353aedab0881458033164f7dca98718
mousa3 <mousa.khoury.1998@gmail.com>
2024-09-08 03:19:45 +0300

    test the pipeline

--- src/components/Hero/HeroContent.jsx
+++ src/components/Hero/HeroContent.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 import Button from "../../UI/Button";
 
 export default function HeroContent() {
   return (
     <div className="home-hero__content">
-      <h1 className="heading-primary">Hey, I'm Mousa Khouri</h1>
+      <h1 className="heading-primary">Hey, I'm Mousa Khoury</h1>
       <div className="home-hero__info">
         <p className="text-primary">A Full Stack Web Developer building both the front-end and back-end of websites and web applications, contributing to the overall success of the product.</p>
       </div>
