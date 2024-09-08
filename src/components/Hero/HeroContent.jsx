952a247a242dc95020a912c5a419b604744694ab
mousa3 <mousa.khoury.1998@gmail.com>
2024-09-08 03:17:16 +0300

    test the pipeline

--- src/components/Hero/HeroContent.jsx
+++ src/components/Hero/HeroContent.jsx
@@ -3,7 +3,7 @@ import Button from "../../UI/Button";
 export default function HeroContent() {
   return (
     <div className="home-hero__content">
-      <h1 className="heading-primary">Hey, I'm Mousa Khoury</h1>
+      <h1 className="heading-primary">Hey, I'm Mousa Khouri</h1>
       <div className="home-hero__info">
         <p className="text-primary">A Full Stack Web Developer building both the front-end and back-end of websites and web applications, contributing to the overall success of the product.</p>
       </div>
